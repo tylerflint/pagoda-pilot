182cfd432b9c84a78139d1cafbbccb52968afe1c
Tyler Flint <tylerflint@Delorum10.local>
2010-12-10 14:06:54 -0700

    changing the file

--- write.php
+++ write.php
@@ -1,6 +1,6 @@
 <?php
 
-$myFile = "test/testFile.txt";
+$myFile = "test2/testFile.txt";
 $fh = fopen($myFile, 'w') or die("can't open file");
 $stringData = "Bobby Bopper\n";
 fwrite($fh, $stringData);
