6a54a7bade06945c808b731684fc01e937a56db3
Tyler Flint <tylerflint@Delorum10.local>
2010-12-10 13:46:59 -0700

    adding a write utility

---
+++ write.php
@@ -0,0 +1,11 @@
+<?php
+
+$myFile = "test/testFile.txt";
+$fh = fopen($myFile, 'w') or die("can't open file");
+$stringData = "Bobby Bopper\n";
+fwrite($fh, $stringData);
+$stringData = "Tracy Tanner\n";
+fwrite($fh, $stringData);
+fclose($fh);
+
+echo "I wrote stuff";
