4f46239da9a3cab7c464ff21ff51cb1c842505f3
Tyler Flint <tylerflint@Delorum10.local>
2011-01-27 19:57:12 -0700

    adding an additional link

--- index.php
+++ index.php
@@ -5,3 +5,6 @@ echo "you're still AMAZING.";
 
 echo "<br /><br />";
 echo "<a href='/info.php'>Check out the phpinfo</a>";
+
+echo "<br /><br />";
+echo "<a href='/write.php'>Check out how I can write to a file</a>";
