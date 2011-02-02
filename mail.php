d7b8226e9bfc464364535aa643d51fd46f99230f
Tyler Flint <tylerflint@Delorum10.local>
2011-02-02 11:01:33 -0700

    adding a to

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 <?php
 
-mail('caffeinated@example.com', 'Test Subject', "This is an email message from the pongo app");
+mail('tylerflint@gmail.com', 'Test Subject', "This is an email message from the pongo app");
 
 echo "email sent";
