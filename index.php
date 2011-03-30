54a4c98630fff2364dd30839b93b7ae7590b6058
tylerflint <tylerflint@gmail.com>
2011-03-30 14:54:52 -0600

    testing complex branch forking

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-<?php
-echo "checking deploy to commit<br />";
-echo "pagoda-pilot <br />";  
-echo "hello PAGODAbox! <br />";
-echo "you're pretty much AMAZING.";
-
-echo "also github is awesome";
-
-echo "<br /><br />";
-echo "<a href='/info.php'>Check out the phpinfo</a>";
-
-echo "<br /><br />";
-echo "<a href='/write.php'>Check out how I can write to a file</a>";
+<h1>WHAT WHAT!</h1>
