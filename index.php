3ac116ddb41cd3d5b4d282014ddc53d99fb7352a
Tyler Flint <tylerflint@gmail.com>
2011-10-10 21:24:10 -0600

    making index.php climactic

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ echo "<br />";
 echo "NEWPAGODABOX YEAHHHH!";
 echo "<br />";
 echo "please tell me we can handle concurrency...<br />";
-echo "and that's a yes!<br />";
+echo "and that's a ... yes!<br />";
   
 ?>
