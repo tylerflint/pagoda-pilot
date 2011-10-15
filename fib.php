abf91fffa5b41b03bf3454edeaf0d3a7f0d77381
Tyler Flint <tylerflint@gmail.com>
2011-10-15 10:02:26 -0600

    adding fobonacci sequence

---
+++ fib.php
@@ -0,0 +1,14 @@
+<?php
+
+function fib( $n ) {
+  // PRE: $n >= 0
+  if( $n < 2 ) {
+    return $n;
+  } else {
+    return fib( $n-1 ) + fib( $n-2 );
+  }
+} 
+
+for( $i=0; $i<5000; $i++ ) {   
+  echo fib( $i )." - ";
+} 
